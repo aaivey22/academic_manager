3955a81e46a04130751696ce9d466f542b4bc0e2
aaivey22 <angelawings22@gmail.com>
2022-12-18 17:56:22 -0500

    first commit

---
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/MainActivity.java
@@ -0,0 +1,14 @@
+package angelaivey.example.academic_schedule_and_progress_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
