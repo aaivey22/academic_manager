ec4181d6d3735e2872989be01d1cc2b0c1e67cb7
aaivey22 <angelawings22@gmail.com>
2022-12-29 19:29:19 -0500

    Added term details activity to ui package. The floating action btn on the term list page now displays text on the term details page when clicked.

---
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/TermDetails.java
@@ -0,0 +1,16 @@
+package angelaivey.example.academic_schedule_and_progress_tracker.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import angelaivey.example.academic_schedule_and_progress_tracker.R;
+
+public class TermDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_details);
+    }
+}
--- app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/TermList.java
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/TermList.java
@@ -2,7 +2,12 @@ package angelaivey.example.academic_schedule_and_progress_tracker.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import angelaivey.example.academic_schedule_and_progress_tracker.R;
 
@@ -12,5 +17,13 @@ public class TermList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TermList.this, TermDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
