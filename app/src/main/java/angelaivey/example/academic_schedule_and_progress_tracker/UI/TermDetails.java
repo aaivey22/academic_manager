dea72074b3348a155514937f8a68d76e96f848d9
aaivey22 <angelawings22@gmail.com>
2023-01-02 13:57:04 -0500

    Term details layout now contains a floating action btn

--- app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/TermDetails.java
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/TermDetails.java
@@ -2,7 +2,11 @@ package angelaivey.example.academic_schedule_and_progress_tracker.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import angelaivey.example.academic_schedule_and_progress_tracker.R;
 
@@ -12,5 +16,13 @@ public class TermDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TermDetails.this, TermList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
