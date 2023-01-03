780805a85dd783effafac49ebe7b5bdd9d325793
aaivey22 <angelawings22@gmail.com>
2023-01-03 17:53:40 -0500

    Built assessment class in dao.

--- app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/dao/AssessmentDAO.java
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/dao/AssessmentDAO.java
@@ -1,4 +1,25 @@
 package angelaivey.example.academic_schedule_and_progress_tracker.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Assessment;
+
+@Dao
 public interface AssessmentDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Assessment assessment);
+    @Update
+    void update(Assessment assessment);
+    @Delete
+    void delete(Assessment assessment);
+    @Query("SELECT * FROM ASSESSMENTS ORDER BY assessmentID ASC")
+    List<Assessment> getAllAssessments();
+
 }
