142b7dd7f62dc438461b5372dd08e068bddbb6e5
aaivey22 <angelawings22@gmail.com>
2023-01-04 18:49:14 -0500

    Added a menu item with sample data. The aplication is running successfully at this point.

--- app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/MainActivity.java
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import angelaivey.example.academic_schedule_and_progress_tracker.Database.Repository;
 import angelaivey.example.academic_schedule_and_progress_tracker.R;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Course;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Term;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
+        Term term = new Term(0, "Term 1", "02/15/2018", "02/01/2023");
+        Repository repository = new Repository(getApplication());
+        repository.insert(term);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemsSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addSampleData:
+            Term term = new Term(0, "Term 1", "02/15/2018", "02/01/2023");
+            Repository repository = new Repository(getApplication());
+            repository.insert(term);
+            Course course = new Course(0, "Maths", "03/0/3029", "03/04/2039", "pass", "Reishi Grizwald", "306-435-7957", "reishi@email.com", 1);
+            repository.insert(course);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
