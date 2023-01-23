4e6eb4a21ac080f39a7db4e380b2736d17c5a2d4
aaivey22 <angelawings22@gmail.com>
2023-01-23 17:24:11 -0500

    Typo corrections to assessment files.

---
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/AssessmentAdapter.java
@@ -0,0 +1,99 @@
+package angelaivey.example.academic_schedule_and_progress_tracker.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import angelaivey.example.academic_schedule_and_progress_tracker.R;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Assessment;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Course;
+
+public class AssessmentAdapter extends RecyclerView.Adapter<AssessmentAdapter.AssessmentViewHolder> {
+    class AssessmentViewHolder extends RecyclerView.ViewHolder {
+        private final TextView assessmentItemTitle;
+        private final TextView assessmentItemType;
+        private final TextView assessmentItemStartDate;
+        private final TextView assessmentItemEndDate;
+
+        private AssessmentViewHolder(View itemView) {
+            super(itemView);
+            assessmentItemTitle = itemView.findViewById(R.id.textViewassessmenttitle);
+            assessmentItemType = itemView.findViewById(R.id.textViewassessmenttype);
+            assessmentItemStartDate = itemView.findViewById(R.id.textViewassessmentstart);
+            assessmentItemEndDate = itemView.findViewById(R.id.textViewassessmentend);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Assessment current = mAssessments.get(position);
+                    Intent intent = new Intent(context, CourseDetails.class);
+                    intent.putExtra("id", current.getAssessmentID());
+                    intent.putExtra("title", current.getAssessmentTitle());
+                    intent.putExtra("type", current.getAssessmentType());
+                    intent.putExtra("start", current.getAssessmentStartDate());
+                    intent.putExtra("end", current.getAssessmentEndDate());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private List<Assessment> mAssessments;
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    public AssessmentAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AssessmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.assessment_list_item, parent, false);
+        return new AssessmentViewHolder((itemView));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentAdapter.AssessmentViewHolder holder, int position) {
+        if (mAssessments != null) {
+            Assessment current = mAssessments.get(position);
+            String title = current.getAssessmentTitle();
+            String type = current.getAssessmentType();
+            String start = current.getAssessmentStartDate();
+            String end = current.getAssessmentEndDate();
+
+            holder.assessmentItemTitle.setText(title);
+            holder.assessmentItemType.setText(type);
+            holder.assessmentItemStartDate.setText(start);
+            holder.assessmentItemEndDate.setText(end);
+
+        } else {
+            holder.assessmentItemTitle.setText("No Assessment Title");
+            holder.assessmentItemType.setText("No Assessment Type");
+            holder.assessmentItemStartDate.setText("No Assessment Start Date");
+            holder.assessmentItemEndDate.setText("No Assessment End Date");
+        }
+
+    }
+
+    public void setAssessments(List<Assessment> assessments) {
+        mAssessments = assessments;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAssessments.size();
+    }
+
+}
--- app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/AssessmentList.java
+++ app/src/main/java/angelaivey/example/academic_schedule_and_progress_tracker/UI/AssessmentList.java
@@ -1,16 +1,55 @@
 package angelaivey.example.academic_schedule_and_progress_tracker.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+import angelaivey.example.academic_schedule_and_progress_tracker.Database.Repository;
 import angelaivey.example.academic_schedule_and_progress_tracker.R;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Assessment;
+import angelaivey.example.academic_schedule_and_progress_tracker.entities.Course;
 
 public class AssessmentList extends AppCompatActivity {
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_list);
+        RecyclerView recyclerView = findViewById(R.id.assessmentrecyclerview);
+        final AssessmentAdapter assessmentAdapter = new AssessmentAdapter(this);
+        recyclerView.setAdapter(assessmentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        repository = new Repository(getApplication());
+        List<Assessment> allAssessments = repository.getAllAssessments();
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton4);
+        assessmentAdapter.setAssessments(allAssessments);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AssessmentList.this, AssessmentDetails.class);
+                startActivity(intent);
+            }
+        });
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Assessment> allAssessments = repository.getAllAssessments();
+        RecyclerView recyclerView = findViewById(R.id.assessmentrecyclerview);
+        final AssessmentAdapter assessmentAdapter = new AssessmentAdapter(this);
+        recyclerView.setAdapter(assessmentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        assessmentAdapter.setAssessments(allAssessments);
+    }
+
 }
